a3c65a6cdfd02544f4531ed00d22ca3ee8e79d2e
pengjinfu <18317916706@163.com>
2018-08-24 15:31:31 +0800

    在manage.py里定义了配置类，开启了debug模式，设置了MySQL数据库，redis数据库，并为之创建了app,db,redis_store对象

--- manage.py
+++ manage.py
@@ -2,3 +2,44 @@
 # -*- coding:utf-8 -*-
 # Author:Dreamer
 # Time:2018.8.16
+import redis as redis
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import redis
+
+# 定义配置类，并从中加载配置
+class Config(object):
+    """工程配置信息"""
+    # 开启debug模式
+    DEBUG = True
+
+    # 配置MySQL数据库
+    # 设置数据库的连接
+    SQLALCHEMY_DATABASE_URI = "mysql://root:mysql@127.0.0.1:3306/information"
+    # 关闭数据库的跟踪
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    # 配置redis数据库
+    # 配置redis的连接
+    REDIS_HOST = "127.0.0.1"
+    # 配置redis的端口
+    REDIS_PORT = 6379
+    # 配置redis的第2个空间存储
+    REDIS_NUM = 2
+
+
+
+# 创建flask对象
+app = Flask(__name__)
+# 把配置信息注册到app里
+app.config.from_object(Config)
+# 创建Mysql对象
+db = SQLAlchemy(app)
+# 创建redis对象
+redis_store = redis.StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
+
+if __name__ == '__main__':
+    app.run()
+
+
+
