1a46171b0b4ec36d49c0b1c663fbbb5d99a07843
pengjinfu <18317916706@163.com>
2018-08-24 15:59:42 +0800

    对session进行配置，并将 session 数据保存到 Redis 中

--- manage.py
+++ manage.py
@@ -11,6 +11,8 @@ from flask_wtf.csrf import CSRFProtect
 # 出现MySQLdb导入错误的时候调用下面导入方式
 import pymysql
 # 使用install_as_MySQLdb函数将pymysql MySQLdb一起使用
+from redis import StrictRedis
+
 pymysql.install_as_MySQLdb()
 # 定义配置类，并从中加载配置
 class Config(object):
@@ -31,6 +33,20 @@ class Config(object):
     REDIS_PORT = 6379
     # 配置redis的第2个空间存储
     REDIS_NUM = 2
+
+    # 设置加密字符串
+    SECRET_KEY = "/W73UULUS4UFO5omviuVZz6+Bcjs5+2nRdvmyYNq1wEryZsMeluALSDGxGnuYoKX"
+    # 调整session存储位置(存储到redis)
+    # 指明sesion存储到那种类型的数据库
+    SESSION_TYPE = "redis"
+    # 上面的指明的数据库的实例对象
+    SESSION_REDIS = StrictRedis(host=REDIS_HOST, port=REDIS_PORT, db=REDIS_NUM)
+    # session数据需要加密
+    SESSION_USE_SIGNER = True
+    # 不设置永久存储
+    SESSION_PERMANENT = False
+    # 默认存储的有效时长 （没有调整之前默认值是timedelta(days=31)）
+    PERMANENT_SESSION_LIFETIME = 86400 * 2
 
 
 
